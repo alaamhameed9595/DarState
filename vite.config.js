bf749827c44aaf275343d5c5885ef8f70ef6fff2
alaamhameed9595 <alaamhameed9595@gmail.com>
2025-07-08 12:21:58 +0300

    fix vite error docker

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import * as path from 'node:path'; // ✅ use `node:path` for native modules in ESM
+import { fileURLToPath } from 'node:url'; // ✅ required in ESM mode
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
     plugins: [
